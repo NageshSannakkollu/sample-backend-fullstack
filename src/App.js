bcc6a52d95f2958e4be9f77ee259e3b65f579c91
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2024-11-21 18:00:52 +0530

    Sample Backend Fullstack

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+  const [value,setValue] = useState([])
+  useEffect(() => {
+    const getHello = async() => {
+      const response = await fetch("https://majestic-daifuku-5be413.netlify.app/")
+      const data = await response.json();
+      setValue(data)
+    }
+    getHello()
+  },[])
+  console.log(value.message)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>{value.message}</div>
+  )
 }
 
-export default App;
+export default App
